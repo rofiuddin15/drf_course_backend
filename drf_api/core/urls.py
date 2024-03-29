f20f31f9573e624f87b7eb25101a4e440cc4bcc0
rofiareiv <kambing.rofi@gmail.com>
2024-03-29 12:59:48 +0700

    view class base.

--- drf_api/core/urls.py
+++ drf_api/core/urls.py
@@ -5,6 +5,8 @@ from . import views
 urlpatterns = [
     path('kategori/', views.kategori_list),
     path('kategori/<int:pk>/', views.kategori_detail),
+    path('produk/', views.ProdukList.as_view()),
+    path('produk/<int:pk>/', views.ProdukDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- drf_api/core/views.py
+++ drf_api/core/views.py
@@ -1,6 +1,8 @@
 from rest_framework import status, permissions
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
+from django.http import Http404
+from rest_framework.views import APIView
 from .models import Kategori, Produk
 from .serializers import KategoriSerializer, ProdukiSerializer
 
@@ -44,4 +46,51 @@ def kategori_detail(request, pk, format=None):
 
     elif request.method == 'DELETE':
         kategori.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+# view untuk produk dengan class base view
+class ProdukList(APIView):
+    """
+    ini merupakan proses pengambilan data atau simpan data
+    """
+    permission_classes = [permissions.AllowAny]
+    def get(self, request, format=None):
+        produk = Produk.objects.all()
+        serializer = ProdukiSerializer(produk, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request, format=None):
+        serializer = ProdukiSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class ProdukDetail(APIView):
+    """
+    ambil data, edit atau hapus data
+    """
+    parser_classes = [permissions.AllowAny]
+    def get_object(self, pk):
+        try:
+            return Produk.objects.get(pk=pk)
+        except Produk.DoesNotExist:
+            raise Http404
+        
+    def get(self, request, pk, format=None):
+        produk = self.get_object(pk)
+        serializer = ProdukiSerializer(produk)
+        return Response(serializer.data)
+    
+    def put(self, request, pk, format=None):
+        produk = self.get_object(pk)
+        serializer = ProdukiSerializer(produk, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, pk, format=None):
+        produk = self.get_object(pk=pk)
+        produk.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
